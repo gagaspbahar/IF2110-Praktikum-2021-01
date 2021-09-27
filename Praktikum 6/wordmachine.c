fdc98f0026dc9d6beb6bfdb9819f4e506139626a
gagaspbahar <gagaspbahar@gmail.com>
2021-09-28 02:54:22 +0700

    Pra Praktikum 6

---
+++ Praktikum 6/wordmachine.c
@@ -0,0 +1,61 @@
+#include "boolean.h"
+#include "charmachine.h"
+#include "wordmachine.h"
+#include <stdio.h>
+
+void ignoreBlank()
+/* Mengabaikan satu atau beberapa BLANK
+   I.S. : currentChar sembarang 
+   F.S. : currentChar ≠ BLANK atau currentChar = MARK */
+{
+    while (currentChar == BLANK){
+        adv();
+    }
+}
+void startWord()
+/* I.S. : currentChar sembarang 
+   F.S. : endWord = true, dan currentChar = MARK; 
+          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
+          currentChar karakter pertama sesudah karakter terakhir kata */
+{
+    start();
+    ignoreBlank();
+    if (currentChar == MARK){
+        endWord = true;
+    }
+    else{
+        endWord = false;
+        copyWord();
+    }
+}
+void advWord()
+/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
+   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
+          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika currentChar = MARK, endWord = true.		  
+   Proses : Akuisisi kata menggunakan procedure copyWord */
+{
+    ignoreBlank();
+    if(currentChar == MARK){
+        endWord = true;
+    }
+    else{
+        copyWord();
+    }
+}
+void copyWord()
+/* Mengakuisisi kata, menyimpan dalam currentWord
+   I.S. : currentChar adalah karakter pertama dari kata
+   F.S. : currentWord berisi kata yang sudah diakuisisi; 
+          currentChar = BLANK atau currentChar = MARK; 
+          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
+{
+    int i = 0;
+    while(currentChar != MARK || currentChar != BLANK){
+        currentWord.contents[i] = currentChar;
+        adv();
+        i++;
+    }
+    currentWord.length = i;
+}
